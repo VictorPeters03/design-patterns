8858f1e2759fd4aee119c44f9761c579f2867c00
VictorP03 <crapowner321@gmail.com>
2024-02-05 21:02:18 +0100

    added docker and project with hello world!

---
+++ design-patterns-application/src/main/java/org/example/designpatternsapplication/DesignPatternsApplication.java
@@ -0,0 +1,15 @@
+package org.example.designpatternsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsApplication.class, args);
+
+		// Add Hello, World!
+		System.out.println("Hello, World!");
+	}
+}
